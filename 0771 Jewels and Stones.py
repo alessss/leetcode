e603fdd6310ed779ca66649162e24fec9854e7c5
alessss <a.makarevich@hotmail.com>
2024-02-20 22:23:09 +0200

    Create 0771 Jewels and Stones.py

---
+++ 0771 Jewels and Stones.py
@@ -0,0 +1,7 @@
+#https://leetcode.com/problems/jewels-and-stones/
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        sum = 0
+        for i in jewels:
+            sum += stones.count(i)
+        return sum
